9d3f18e2e8067927f36dcc9d89b667bf0c390b5e
Maas Lalani <maas@live.ca>
2017-05-02 18:21:42 -0400

    Advanced querying features
    Instead of storing key/value pairs, cookiedb allows you to store objects and query them based on a subset of properties, much like mongodb.

--- cookiedb.js
+++ cookiedb.js
@@ -1,15 +1,27 @@
 class CookieDB {
 
-  insert(key, value) {
-    localStorage.setItem(key, JSON.stringify(value))
+  insert(value) {
+    value._id = localStorage.length
+    localStorage.setItem(localStorage.length, JSON.stringify(value))
   }
 
-  find(key) {
-    return JSON.parse(localStorage.getItem(key))
+  find(query) {
+    let items = []
+
+    for (let i = 0; i < localStorage.length; i++)
+      items.push(JSON.parse(localStorage[i]))
+
+    for (let property in query)
+      items = items.filter(item => item[property] == query[property])
+
+    return items
   }
 
-  remove(key) {
-    localStorage.removeItem(key)
+  remove(query) {
+    let items = this.find(query)
+
+    for (let i = 0; i < items.length; i++)
+      localStorage.removeItem(items[i]._id)
   }
 
   update(key, value) {
@@ -19,6 +31,14 @@ class CookieDB {
 
   count() {
     return localStorage.length
+  }
+
+  dump() {
+    return localStorage
+  }
+
+  drop() {
+    localStorage.clear()
   }
 
 }
